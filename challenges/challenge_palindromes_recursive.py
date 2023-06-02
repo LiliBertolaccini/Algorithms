51384763a0309661a971aa6b9938775dea33a976
Lilibertola <bertolaccini55@gmail.com>
2023-06-01 21:26:15 -0300

    ok req 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,17 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    #word_list = ["word", "low_index", "high_index"]
+    if word == "":
+        return False
 
-# def check_palindromes(words):
-#    lista = []
-#    for word in words:
-#        check_palindromes = is_palindrome_recursive(word)
-#        lista.append(check_palindromes)
-#    return list
-
-#word_list = ["amor", "prova", "ola"]
-#palindrome_results = check_palindromes(word_list)
-
-#print(palindrome_results)
     if low_index >= high_index:
         return True
 
@@ -19,17 +9,3 @@ def is_palindrome_recursive(word, low_index, high_index):
         return False
 
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-
-def check_palindromes(words):
-    list = []
-    for word in words:
-        check_palindromes = is_palindrome_recursive(word, 0, len(word) - 1)
-        list.append(check_palindromes)
-    return list
-
-word_list = ["amor", "prova", "ola"]
-palindrome_results = check_palindromes(word_list)
-
-print(palindrome_results)
-    #"""Faça o código aqui."""
-raise NotImplementedError
