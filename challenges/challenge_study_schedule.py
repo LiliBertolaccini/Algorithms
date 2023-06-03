c6a72681768f5e5b9c2c7f1274fb40ede4b84856
Lilibertola <bertolaccini55@gmail.com>
2023-06-03 18:56:47 -0300

    ok req 1 refatorado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,8 @@
 def study_schedule(permanence_period, target_time):
     students_present = 0
+    if not isinstance(target_time, int):
+        return None
     for start, end in permanence_period:
-        if not isinstance(target_time, int):
-            return None
         if not isinstance(start, int) or not isinstance(end, int):
             return None
         if start <= target_time <= end:
