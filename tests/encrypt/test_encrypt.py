56142d3d2db7da252f82e57fe3a1a5be58d96458
Lilibertola <bertolaccini55@gmail.com>
2023-06-03 20:56:11 -0300

    ok req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("python", "B")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(3, 2)
+
+    assert encrypt_message("python", 9) == "nohtyp"
+    assert encrypt_message("python", 2) == "noht_yp"
