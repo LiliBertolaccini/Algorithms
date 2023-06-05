a0156ba7fa27570323087d045a0b54177a5c979a
Lilibertola <bertolaccini55@gmail.com>
2023-06-05 14:28:52 -0300

    05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,29 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    #if not isinstance(nums, int):
+    #    return False
+    
+    #if len(nums) < 2:
+    #    return False
+    
+    #if any(num <= 0 for num in nums):
+    #    return False
+    
+    #nums.sort()
+
+    #for i in range(1, len(nums)):
+    #    if nums[i] == nums[i - 1]:
+    #        return nums[i]
+        
+    #return False
+
+    if (not nums or any(not isinstance(num, int) for num in nums)
+        or len(nums) < 2 or any(num <= 0 for num in nums)):
+        return False
+
+    nums.sort()
+
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i - 1]:
+            return nums[i]
+
+    return False
